95473a06c250254a951311ca49bb552ef0cd821b
Asmatchd <Asmatchd@gmail.com>
2021-09-22 18:35:11 +0500

    project started

---
+++ App.js
@@ -0,0 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: '#faf',
+        height: '100%',
+      }}>
+      <Text
+        style={{
+          color: 'red',
+          fontSize: 25,
+        }}>
+        Study
+      </Text>
+    </View>
+  );
+};
+
+export default App;
